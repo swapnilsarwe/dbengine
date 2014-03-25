ed02b2532b072bd8487cf2c2373a05f08d22029d
Swapnil Sarwe <swapnilsarwe@gmail.com>
2014-03-26 02:09:24 +0530

    added comments

--- index.php
+++ index.php
@@ -8,12 +8,17 @@ $arrConfig = array(
         'database' => 'database',
         'port' => '3306'
 );
+// PDO Example
 $db = DBFactory::Create('pdo', $arrConfig);
 $arrResult = $db->getData('SELECT * FROM tablename LIMIT 5');
 print_r($arrResult);
+
+// MySQLi Example
 $db = DBFactory::Create('mysqli', $arrConfig);
 $arrResult = $db->getData('SELECT * FROM tablename LIMIT 5');
 print_r($arrResult);
+
+// MySQL Example
 $db = DBFactory::Create('mysql', $arrConfig);
 $arrResult = $db->getData('SELECT * FROM tablename LIMIT 5');
 print_r($arrResult);
